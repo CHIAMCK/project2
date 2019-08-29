d5af1083a43fe3eb1e6626fa3b8fc152949e94ac
CHIAM CHING KOK <chiamck91@hotmail.com>
2019-08-29 15:41:53 +0800

    added some comments

--- vouchers/forms/voucher_check.py
+++ vouchers/forms/voucher_check.py
@@ -9,11 +9,13 @@ class CheckVoucherForm(forms.Form):
 
     def clean_voucher_code(self):
         voucher_code = self.cleaned_data['voucher_code']
+        # if voucher code is not in the DB, it is invalid
         try:
             self.voucher = Voucher.objects.get(code__exact=voucher_code)
         except Voucher.DoesNotExist:
             raise ValidationError('Voucher code is invalid.')
 
+        # if the voucher is used more than 3 times, voucher code is invalid
         if self.voucher.usage_limit <= 0:
             raise ValidationError('Voucher code is invalid.')
         return voucher_code
--- vouchers/views/check_voucher.py
+++ vouchers/views/check_voucher.py
@@ -5,12 +5,16 @@ from ..forms.voucher_check import CheckVoucherForm
 
 
 class CheckVoucherView(FormView):
+    ''' A view to check the validity of voucher'''
+    
     form_class = CheckVoucherForm
     template_name = 'voucher_check.html'
 
     def form_valid(self, form):
+        # record the usage of voucher
         form.voucher.usage_limit = form.voucher.usage_limit - 1
         form.voucher.save(update_fields=['usage_limit'])
+        # render the form with voucher code and discount value
         return render(self.request, self.template_name,
                       context=self.get_context_data(
                       form=form, discount=form.voucher.discount))
