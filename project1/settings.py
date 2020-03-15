26ffea2dc7049700cd7e57a0b57579bd67b9a25a
CHIAM CHING KOK <chiamck91@hotmail.com>
2020-03-15 13:21:54 +0800

    minor change

--- project1/settings.py
+++ project1/settings.py
@@ -109,7 +109,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
@@ -122,7 +121,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
